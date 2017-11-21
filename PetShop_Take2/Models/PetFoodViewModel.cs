a52d66a48dcb02289eb72596fa7afd7dd33a1df6
McShi <rashida.mcwilliams@gmail.com>
2017-11-21 14:08:15 -0500

    Created new modelview

---
+++ PetShop_Take2/Models/PetFoodViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PetShop_Take2.Models
+{
+    public class PetFoodViewModel
+    {
+        public Pet pet { get; set; }
+        public Food food { get; set; }
+    }
+}
